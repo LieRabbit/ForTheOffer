a1f33db631c9cdfc6c27820bfc651cd04124e556
LieRabbit <lierabbitxyy@gmail.com>
2018-03-04 13:56:47 +0800

    添加别的实现方法

--- src/从尾到头打印链表.java
+++ src/从尾到头打印链表.java
@@ -1,11 +1,28 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 /**
  * 输入一个链表，从尾到头打印链表每个节点的值。
  */
 public class 从尾到头打印链表 {
+
     public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> res = new ArrayList<>();
+
+        printListFromTailToHead(listNode, res);
+
+        return res;
+    }
+
+    public void printListFromTailToHead(ListNode node, List<Integer> res) {
+        if (node != null) {
+            printListFromTailToHead(node.next, res);
+            res.add(node.val);
+        }
+    }
+
+    public ArrayList<Integer> printListFromTailToHead2(ListNode listNode) {
         ArrayList<Integer> res = new ArrayList<>();
         Stack<ListNode> stack = new Stack<>();
         ListNode node = listNode;
@@ -19,5 +36,32 @@ public class 从尾到头打印链表 {
         }
 
         return res;
+    }
+
+    public ArrayList<Integer> printListFromTailToHead3(ListNode listNode) {
+        ArrayList<Integer> res = new ArrayList<>();
+
+        ListNode node = reverseList(listNode);
+
+        while (node != null) {
+            res.add(node.val);
+            node = node.next;
+        }
+
+        return res;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode next;
+
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+
+        return pre;
     }
 }
