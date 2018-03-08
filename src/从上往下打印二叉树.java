928efe6d2b1a149a96ff7a9b922ab02d7e360e00
LieRabbit <lierabbitxyy@gmail.com>
2018-03-08 11:09:08 +0800

    从上往下打印二叉树

---
+++ src/从上往下打印二叉树.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 从上往下打印出二叉树的每个节点，同层节点从左至右打印。
+ */
+public class 从上往下打印二叉树 {
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        TreeNode node;
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root != null)
+            queue.add(root);
+        ArrayList<Integer> res = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            node = queue.poll();
+            if (node.left != null)
+                queue.add(node.left);
+            if (node.right != null)
+                queue.add(node.right);
+            res.add(node.val);
+        }
+
+        return res;
+    }
+}
