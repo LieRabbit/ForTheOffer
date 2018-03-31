73703d9c835210643c9791a63205efd8bfaea67b
LieRabbit <lierabbitxyy@gmail.com>
2018-03-31 23:37:35 +0800

    构建乘积数组

--- src/构建乘积数组.java
+++ src/构建乘积数组.java
@@ -3,8 +3,12 @@
  */
 public class 构建乘积数组 {
     public int[] multiply(int[] A) {
+        if (A == null)
+            return null;
         int length = A.length;
         int[] B = new int[length];
+        if (length == 0)
+            return B;
         B[0] = 1;
         for (int i = 1; i < length; i++)
             B[i] = B[i - 1] * A[i - 1];
