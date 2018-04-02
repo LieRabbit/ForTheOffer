efe7b96f3997c34eb5d3b7de6ffa20283f505120
LieRabbit <lierabbitxyy@gmail.com>
2018-04-02 18:28:40 +0800

    添加注释

--- src/表示数值的字符串.java
+++ src/表示数值的字符串.java
@@ -4,24 +4,25 @@
  */
 public class 表示数值的字符串 {
     public boolean isNumeric(char[] str) {
+        // 标记符号、小数点、e是否出现过
         boolean hasSign = false, hasDecimal = false, hasE = false;
         for (int i = 0, len = str.length; i < len; i++) {
             char ch = str[i];
             if (ch == '+' || ch == '-') {
-                if (hasSign && str[i - 1] != 'e' && str[i - 1] != 'E')
+                if (hasSign && str[i - 1] != 'e' && str[i - 1] != 'E') // 第二次出现+-符号，则必须紧接在e之后
                     return false;
-                else if (!hasSign && i > 0 && str[i - 1] != 'e' && str[i - 1] != 'E')
+                else if (!hasSign && i > 0 && str[i - 1] != 'e' && str[i - 1] != 'E') // 第一次出现+-符号，且不是在字符串开头，则也必须紧接在e之后
                     return false;
                 hasSign = true;
             } else if (ch == 'e' || ch == 'E') {
-                if (hasE || i == len - 1)
+                if (hasE || i == len - 1) // 不能同时存在两个e且e后面一定要接数字
                     return false;
                 hasE = true;
             } else if (ch == '.') {
-                if (hasE || hasDecimal)
+                if (hasE || hasDecimal) // e后面不能接小数点，小数点不能出现两次
                     return false;
                 hasDecimal = true;
-            } else if (ch < '0' || ch > '9')
+            } else if (ch < '0' || ch > '9') // 不合法字符
                 return false;
         }
 
